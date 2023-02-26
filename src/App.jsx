732f9528efbd57a1173f0c3940c1446a75f864e3
JayThiraphat <wongwatnicha@gmail.com>
2023-02-27 06:08:56 +0700

    commit

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+import './App.css'
+import { v4 as uuidv4 } from 'uuid';
+import List from './components/List';
+import Alert from './components/Alert';
+
+function App() {
+  const [name,setName] = useState("")
+  const [list,setList] = useState([])
+
+  const [alert,setAlert] = useState({show:false,msg:'',type:''})
+  const [checkEditItem,setCheckEditItem] = useState(false)
+  const [editId,setEditId] = useState(null)
+
+  const submitData =(e)=>{
+    e.preventDefault()
+    if(!name){
+      //show alert
+      setAlert({show:true,msg:'กรุณาป้อนข้อมูล',type:'error'})
+    }else if(checkEditItem && name){
+      //update Data
+      const result = list.map((item)=>{
+        if(item.id === editId){
+          return {...item,title:name}
+        }
+        return item
+      })
+      setList(result)
+      setName("")
+      setCheckEditItem(false)
+      setAlert({show:true,msg:"แก้ไขข้อมูลเรียบร้อย",type:"success"})
+    }
+    else{
+        const newItem = {
+          id:uuidv4(),
+          title:name
+        }
+        setList([...list,newItem])
+        setName("")
+        setAlert({show:true,msg:"บันทึกข้อมูลเรียบร้อย",type:"success"})
+      }
+    
+  }
+   
+  const removeItem=(id)=>{
+    const result = list.filter((item)=>item.id !== id)
+    setList(result)
+    setAlert({show:true,msg:"ลบข้อมูลเรียบร้อย",type:"error"})
+  }
+
+  const editItem=(id)=>{
+    console.log(id)
+    setCheckEditItem(true)
+    setEditId(id)
+    const searchItem =  list.find((item)=>item.id === id)
+    console.log(searchItem)
+    setName(searchItem.title)
+  }
+  return (
+    <section className='container'>
+      <h1>TodoList App</h1>
+      {alert.show && <Alert {...alert} setAlert={setAlert} list={list}/>}
+      <form className='form-group' onSubmit={submitData}>
+        <div className="form-control">
+          <input type="text" className='text-input' 
+            onChange={(e)=>setName(e.target.value)}
+            value={name} 
+          />
+          <button type="submit" className='submit-btn'>{checkEditItem ? "แก้ไขข้อมูล" :"เพิ่มข้อมูล"}</button>
+        </div>
+      </form>
+      <section className='list-container'>
+          {list.map((data,index)=>{
+            return <List key={index} {...data} removeItem={removeItem} editItem={editItem}/>
+          })}
+      </section>
+    </section>
+  )
+}
+
+export default App
